18a51f4a91689332052050bdb565f95b46dfb5ed
Carson Myers <carsonscottmyers@gmail.com>
2021-01-15 11:39:07 -0700

    MVP2

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,51 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+
+const articleData = axios.get("https://lambda-times-api.herokuapp.com/articles")
+
+articleData.then((response) =>{
+    const articles = response.data.articles
+    const content = Object.values(articles)
+    content.forEach((element => {
+        element.forEach((ele) =>{
+            createCard(ele)
+        })
+    }))
+})
+
+const cardsContainer = document.querySelector('.cards-container')
+
+function createCard(cardData) {
+
+    const card = document.createElement ('div');
+    const headline = document.createElement ('div');
+    const author = document.createElement ('div');
+    const imgContainer = document.createElement ('div');
+    const img = document.createElement ('img');
+    const authorName = document.createElement ('span');
+
+    card.classList.add("card");
+    headline.classList.add("headline")
+    author.classList.add("author")
+    imgContainer.classList.add("img-container")
+
+    headline.textContent = cardData["headline"]
+    img.src = cardData["authorPhoto"]
+    authorName.textContent = cardData["authorName"]
+
+    cardsContainer.appendChild(card)
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(authorName)
+
+    card.addEventListener("click", event => {
+        console.log(`${event.currentTarget.querySelector("div.headline").textContent}`)
+    })
+
+    return card;
+}
+
